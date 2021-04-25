673bc4d60f0868d091404c9d323a168b0b63ce08
WarmedHeart <40893456+WarmedHeart@users.noreply.github.com>
2021-04-25 08:38:02 +0800

    阻塞公共方法

--- src/utils/util.js
+++ src/utils/util.js
@@ -1,1 +1,13 @@
-export default {}
+export default {
+  /* 延迟函数 （外层函数套有async/await会有阻塞方法内部后续执行的效果）*/
+  sleep(delayTime) {
+    return new Promise((resolve) => {
+      setTimeout(resolve, delayTime)
+    });
+  },
+  /* 阻塞主线程（js属于单线程） */
+  blockMainThread(delayTime) {
+    let startTime = new Date().getTime();
+    while((new Date().getTime() - startTime) < delayTime) {}
+  }
+}
