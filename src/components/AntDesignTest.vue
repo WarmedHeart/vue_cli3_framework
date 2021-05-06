2f786736615e131186c37abb39ed0416d076e68e
郭天航 <guo-tianhang@smtp.telek.com.cn>
2021-05-06 14:01:36 +0800

    测试使用ant-design

---
+++ src/components/AntDesignTest.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="main-wrapper">
+    <a-button type="primary">
+      Primary
+    </a-button>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.main-wrapper {
+  width: 100%;
+  height: 100%;
+}
+</style>
--- src/views/About/About.vue
+++ src/views/About/About.vue
@@ -2,20 +2,23 @@
   <!-- 当不存在moduleName字段，想注册模块，该视图被打开时再进行注册store，再SelfRegisterComp模板有使用 -->
   <div>
     <self-register-comp />
-    <element-test/>
+    <element-test />
+    <ant-design-test />
   </div>
 </template>
 
 <script>
 import SelfRegisterComp from "components/SelfRegisterComp.vue";
 import ElementTest from "components/ElementTest.vue";
+import AntDesignTest from "components/AntDesignTest.vue";
 import aboutStoreObj from "./store.js";
 const MUDULE_NAME = "aboutStore";
 
 export default {
   components: {
     SelfRegisterComp,
-    ElementTest
+    ElementTest,
+    AntDesignTest
   },
   beforeCreate() {
     //注册模块
