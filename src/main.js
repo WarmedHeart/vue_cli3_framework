270598c2d2c5249b7eed0d11ffb2af2bceacdc1a
郭天航 <guo-tianhang@smtp.telek.com.cn>
2021-05-06 14:01:13 +0800

    引入AntDesign

--- src/main.js
+++ src/main.js
@@ -8,11 +8,15 @@ import "./styles/base.css"; // 全局公共样式
 import "./assets/icon_fonts/iconfont.css";  // 全局阿里图标库
 
 import Common from "./utils/util.js"; // 全局公共方法
+
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+import Antd from "ant-design-vue";
+import "ant-design-vue/dist/antd.css";
 
 Vue.prototype.common = Common;
 Vue.use(ElementUI);
+Vue.use(Antd);
 Vue.config.productionTip = process.env.NODE_ENV === 'production'
 
 new Vue({
