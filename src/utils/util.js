476eeb1e13562f9dc89d48e21e457fa65e52f3a2
WarmedHeart <40893456+WarmedHeart@users.noreply.github.com>
2021-06-04 09:30:08 +0800

    新增公共方法-深浅拷贝

--- src/utils/util.js
+++ src/utils/util.js
@@ -9,5 +9,28 @@ export default {
   blockMainThread(delayTime) {
     let startTime = new Date().getTime();
     while((new Date().getTime() - startTime) < delayTime) {}
+  },
+  //克隆对象(浅拷贝)
+  clone(obj) {
+    return Object.assign({}, obj);
+  },
+  //克隆对象(深拷贝)
+  cloneObj(myObj) {
+    if (typeof myObj != "object") {
+      return myObj;
+    }
+    if (myObj == null) {
+      return myObj;
+    }
+    let myNewObj;
+    if (Object.prototype.toString.call(myObj) == "[object Array]") {
+      myNewObj = new Array();
+    } else {
+      myNewObj = new Object();
+    }
+    for (var i in myObj) {
+      myNewObj[i] = this.cloneObj(myObj[i]);
+    }
+    return myNewObj;
   }
 }
